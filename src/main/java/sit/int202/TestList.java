9ce56fb8f36b9d6d94e40ddfc0739a15b93a421e
Chanidapaka <chanidapapanta@gmail.com>
2024-10-26 20:48:27 +0700

    AddFile02 13/08/2567 TestList

--- src/main/java/sit/int202/TestList.java
+++ src/main/java/sit/int202/TestList.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class TestList {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<Integer>(); //สร้าง array ให้ 10ช่อง //จำนวนช่อง สำคัญมาก
+        List<Integer> list = new ArrayList<Integer>(120); //สร้าง array ให้ 10ช่อง //จำนวนช่อง สำคัญมาก //มันเต็มแล้วขยาย1.5 เท่า กว่าจะครบ100 มันขยายหลายรอบมาก
+        for (int i = 0; i < 30; i++){
+            list.add((int) (Math.random()*100));
+        }
     }
 }
