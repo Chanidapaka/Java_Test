351e5442e6299a77f1afbeb92978cd315ff9bc30
Chanidapaka <chanidapapanta@gmail.com>
2024-10-26 21:01:10 +0700

    AddFile03 13/08/2567 Student

--- src/main/java/sit/int202/Student.java
+++ src/main/java/sit/int202/Student.java
@@ -5,39 +5,40 @@ import lombok.*; //ตัวที่ช่วยทำ คือการเอ
 import java.util.Comparator;
 
 //Lombok ช่วยทำ Mathod หมดเลย อยากได้อะไรก็ใส่ได้เลย
-@Setter //คือตัวที่เราอยากได้จาก lombok
+@Setter
 @Getter
-@ToString //คือการไม่อยากได้
-@AllArgsConstructor
-@NoArgsConstructor
+@ToString
+@AllArgsConstructor //สร้างคอนสตรัคเตอร์ (Constructor) ที่มีพารามิเตอร์ครบทุกฟิลด์
+@NoArgsConstructor //สร้างคอนสตรัคเตอร์แบบไม่มีพารามิเตอร์
 
-public class Student implements Comparable<Student> {
-    private Integer id; //เก็บID
-    private String name;  //เก็บชื่อ
-    private Double gpax; //เก็บเกรด
+//การประกาศคลาส
+public class Student implements Comparable<Student> { //Comparable ใช้เปรียบเทียบ
+    private Integer id;
+    private String name;
+    private Double gpax;
 
-
+   //Comparator สำหรับเรียงลำดับโดย GPAX และชื่อ
     public static Comparator<Student> GPAX_NAME_comparator = new Comparator<Student>() {
-
         @Override
-        public int compare(Student s1, Student s2) {
+        public int compare(Student s1, Student s2) { //เป็นเมธอดที่ใช้เปรียบเทียบ gpax ของนักเรียนทั้งสองคน
             double x1 = s1.getGpax();
             double x2 = s2.getGpax();
             if (x1 != x2) {
-                if (x1 > x2) {
+                if (x1 > x2) { //ถ้า gpax ของ s1 มากกว่า s2 จะคืนค่า -1 (บอกว่า s1 มาก่อน)
                     return -1;
-                } else {
+                } else {   //ถ้า gpax ของ s1 น้อยกว่า s2 จะคืนค่า 1 (บอกว่า s2 มาก่อน)
                     return 1;
                 }
             } else {
-                return s1.getName().compareTo(s1.getName());
+                return s1.getName().compareTo(s1.getName()); //า gpax เท่ากัน จะใช้การเปรียบเทียบจาก name แทน โดยเรียงตามลำดับตัวอักษร
             }
         }
     };
 
+    //เมธอด compareTo สำหรับเปรียบเทียบตาม id
         @Override
         public int compareTo(Student o) {
-            return -(this.id - o.id);
+            return -(this.id - o.id); //ใช้การลบเพื่อทำให้เรียงจากมากไปหาน้อย
         }
 
 }
